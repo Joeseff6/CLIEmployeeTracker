d2840e8da11a33e312db53a630e6f86f0f9c0a9a
Joseph Soria <josephsoria6@gmail.com>
2021-02-26 00:05:36 -0600

    Added console.table module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require(`dotenv`).config();
+const consoleTable = require(`console.table`);
 const inquirer = require(`inquirer`);
 const mysql = require(`mysql2`);
 const questions = require(`./questions`);
