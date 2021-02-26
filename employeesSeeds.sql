91438ecbcb38419ef0bdd7c22bd6dd91d4d6d701
Joseph Soria <josephsoria6@gmail.com>
2021-02-26 00:06:00 -0600

    Updated SQL with table creation

---
+++ employeesSeeds.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department(
+id INTEGER AUTO_INCREMENT NOT NULL,
+name VARCHAR(30),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+id INTEGER AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(10,2) NOT NULL,
+department_id INTEGER NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+id INTEGER AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NOT NULL, last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL, manager_id INTEGER NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(role_id) REFERENCES role(id),
+  FOREIGN KEY(manager_id) REFERENCES role(id)
+);
