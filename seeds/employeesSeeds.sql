fe87707569739d60b9c8c4a932a78a136c53e0a0
Joseph Soria <josephsoria6@gmail.com>
2021-03-06 22:23:12 -0600

    Fixed indentation

--- seeds/employeesSeeds.sql
+++ seeds/employeesSeeds.sql
@@ -7,7 +7,7 @@ USE employees_db;
 CREATE TABLE department(
 id INTEGER AUTO_INCREMENT NOT NULL,
 name VARCHAR(30),
-  PRIMARY KEY(id)
+	PRIMARY KEY(id)
 );
 
 CREATE TABLE role(
@@ -15,10 +15,10 @@ id INTEGER AUTO_INCREMENT NOT NULL,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL(10,2) NOT NULL,
 department_id INTEGER NOT NULL,
-  PRIMARY KEY(id),
-  CONSTRAINT fk_department
-  FOREIGN KEY (department_id) 
-  REFERENCES department(id)
+	PRIMARY KEY(id),
+	CONSTRAINT fk_department
+	FOREIGN KEY (department_id) 
+	REFERENCES department(id)
 );
 
 CREATE TABLE employee(
